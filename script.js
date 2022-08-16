88819b176999f57bae9a0871096b38014deb1009
Juliano Fonseca <julianof1796@yahoo.com.br>
2022-08-16 14:14:22 -0300

    Resolvido erros de lint

--- script.js
+++ script.js
@@ -1,22 +1,21 @@
 function userValidation() {
-  const email = document.getElementById("email");
-  const senha = document.getElementById("senha");
+  const email = document.getElementById('email');
+  const senha = document.getElementById('senha');
 
-  if (email.value === "tryber@teste.com" && senha.value === "123456") {
-    alert("Olá, Tryber!");
+  if (email.value === 'tryber@teste.com' && senha.value === '123456') {
+    alert('Olá, Tryber!');
   } else {
-    alert("Email ou senha inválidos.");
+    alert('Email ou senha inválidos.');
   }
-  const button = document.getElementById("btn");
-  button.addEventListener("click", userValidation);
+  const button = document.getElementById('btn');
+  button.addEventListener('click', userValidation);
 }
 
 userValidation();
 
-const btnSubmit = document.querySelector("#submit-btn");
-const concordo = document.querySelector("#agreement");
+const btnSubmit = document.querySelector('#submit-btn');
+const concordo = document.querySelector('#agreement');
 
-concordo.addEventListener('click', enableSubmit);
 function enableSubmit() {
   if (concordo.checked === true) {
     btnSubmit.disabled = false;
@@ -24,3 +23,4 @@ function enableSubmit() {
     btnSubmit.disabled = true;
   }
 }
+concordo.addEventListener('click', enableSubmit);
