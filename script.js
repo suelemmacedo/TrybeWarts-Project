4ec8e63e98a33aaaf29bcb8f2762dd10ab54a065
Marina Padilha <marina.padilha27@gmail.com>
2022-08-16 23:50:43 -0300

    Resolução do requisito 21.

--- script.js
+++ script.js
@@ -40,7 +40,56 @@ text.addEventListener("keyup", function (e) {
   pValor.innerHTML = -charsDigitados + limiteChars;
 });
 
-const nome = document.getElementById("input-name");
+const formData = document.getElementById('form-data');
+const submitName = document.getElementById('submit-name');
+const submitEmail = document.getElementById('submit-email');
+const submitCasa = document.getElementById('submit-casa');
+const submitFamilia = document.getElementById('submit-familia');
+const submitMaterias = document.getElementById('submit-materias');
+const submitAvaliacao = document.getElementById('submit-avaliacao');
+const submitObservacoes = document.getElementById('submit-observacoes');
+
+const form = document.querySelectorAll("main")[0];
+const formDataName = document.getElementById('input-name');
+const formDataLastName = document.getElementById('input-lastname');
+const formDataEmail = document.getElementById('input-email');
+const formDataCasa = document.getElementById('house');
+
+function submitClick(event) {
+  event.preventDefault();
+  form.style.display = 'none';
+  formData.style.display = 'inline-block';
+}
+
+const textArea = document.getElementById('textarea');
+
+function changeFormData() {
+  submitName.innerHTML = `Nome: ${formDataName.value} ${formDataLastName.value}`;
+  submitEmail.innerHTML = `Email: ${formDataEmail.value}`;
+  submitCasa.innerHTML = `Casa: ${formDataCasa.value}`;
+  submitFamilia.innerHTML = `Família: ${document.querySelector('input[name="family"]:checked').value}`;
+  submitAvaliacao.innerHTML = `Avaliação: ${document.querySelector('input[name="rate"]:checked').value}`;
+  submitObservacoes.innerHTML = `Observações: ${textArea.value}`;
+}
+
+function setMaterias() {
+  const formDataMaterias = document.querySelectorAll('input[name="materia"]:checked');
+  let resultado = '';
+  for(let i = 0; i < formDataMaterias.length; i += 1) {
+    if(i === 0) {
+      resultado = formDataMaterias[i].value;
+    } else {
+      resultado += `, ${formDataMaterias[i].value}`;
+    }
+  }
+  submitMaterias.innerHTML = `Matérias: ${resultado}`;
+}
+
+btnSubmit.addEventListener('click', submitClick);
+btnSubmit.addEventListener('click', changeFormData);
+btnSubmit.addEventListener('click', setMaterias);
+
+/* const nome = document.getElementById("input-name");
 const sobrenome = document.getElementById("input-lastname");
 
 let estudantes = [];
@@ -56,20 +105,22 @@ const addEstudante = (ev) => {
     Avaliação: document.getElementById("label-rate").value,
     Observações: document.getElementById("textarea").value,
   };
+  let estudantes = [];
   estudantes.push(estudante);
   document.forms[0].reset();
+  console.log(estudantes);
 };
 
 document.addEventListener("DOMContentLoaded", (e) => {
   e.preventDefault();
   document.getElementById("submit-btn").addEventListener("click", addEstudante);
-});
+}); */
 
-const form = document.querySelectorAll("main")[0];
-form.addEventListener("click", function (e) {
-  e.preventDefault();
-  const newForm = document.createElement("div");
-  newForm.id = "form-data";
-  newForm.innerHTML = estudantes;
-  form.parentNode.replaceChild(newForm, form);
-});
+//const form = document.querySelectorAll("main")[0];
+//form.addEventListener("click", function (e) {
+//  e.preventDefault();
+//  const newForm = document.createElement("div");
+//  newForm.id = "form-data";
+//  newForm.innerHTML = estudantes;
+//  form.parentNode.replaceChild(newForm, form);
+//});
